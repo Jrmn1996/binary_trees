98190901eecff67c712ebf2856e5bf82a687f32f
Jrmn1996 <jrmn1996@gmail.com>
2023-11-30 12:54:54 -0800

    2.1

--- binary_trees.h
+++ binary_trees.h
@@ -41,6 +41,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 void binary_tree_print(const binary_tree_t *);
 size_t measure_height(const binary_tree_t *tree);
@@ -49,5 +50,6 @@ size_t measure_size(const binary_tree_t *tree);
 size_t count_leaves(const binary_tree_t *tree);
 size_t count_nodes(const binary_tree_t *tree);
 int check_children(const binary_tree_t *node);
+int pow(int x, int y);
 
 #endif
